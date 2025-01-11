008f63405986895bdd83d51b93b5e8716ddb7b30
minjeongss <dotoriido@gmail.com>
2025-01-11 22:46:18 +0900

    feat: 빌드를 위한 환경 설정 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { Skeleton } from "./components/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Skeleton width={60} height={60} />
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/index.tsx"),
+      name: "index",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ["react"],
+    },
+  },
   plugins: [react()],
-})
+});
