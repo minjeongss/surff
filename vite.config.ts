fc020cd405f31fb5726059806ae9dcb10fa56ffe
minjeongss <dotoriido@gmail.com>
2025-01-12 16:21:57 +0900

    docs: src > packages로 폴더명 변경

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import dts from "vite-plugin-dts";
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.tsx"),
+      entry: path.resolve(__dirname, "packages/index.tsx"),
       name: "surff",
       fileName: "index",
     },
