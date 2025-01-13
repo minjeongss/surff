94bd3bef28d49b58590686794a93750af034e019
minjeongss <dotoriido@gmail.com>
2025-01-14 01:54:28 +0900

    feat: Portal에 리액트 사이클과 SSR 예외 처리를 고려한 최적화 적용

--- packages/hooks/usePortal.ts
+++ packages/hooks/usePortal.ts
@@ -1,19 +1,41 @@
-type DomNodeId = string;
+import { useEffect } from "react";
 
-const createElement = (selectedId: DomNodeId) => {
+// SSR 예외 처리: SSR에선 브라우저가 없기에, 동작하지 않도록 한다.
+const isBrowser =
+  typeof window !== "undefined" && typeof document !== "undefined";
+
+// ID를 가진 DOM Node를 출력한다.
+const createElement = (selectedId: string) => {
   const element = document.createElement("div");
   element.setAttribute("id", selectedId);
   return element;
 };
 
-const usePortal = (selectedId: DomNodeId) => {
-  const parentElement = document.body;
-  const isExistElement = document.getElementById(selectedId);
-  const element = isExistElement ? isExistElement : createElement(selectedId);
+/**
+ * @description
+ * Portal 사용을 위한 DOM Node 생성 로직
+ *
+ * @param selectedId
+ * @returns HTMLDivElement
+ */
+const usePortal = (selectedId: string) => {
+  const element = isBrowser
+    ? document.getElementById(selectedId) || createElement(selectedId)
+    : null;
 
-  if (!isExistElement) {
-    parentElement.appendChild(element);
-  }
+  useEffect(() => {
+    if (!element) return;
+
+    const parentElement = document.body;
+    if (!document.getElementById(selectedId)) {
+      parentElement.appendChild(element);
+    }
+
+    return () => {
+      parentElement.removeChild(element);
+    };
+  }, []);
+
   return element;
 };
 
