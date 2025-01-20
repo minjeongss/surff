e251cc213df1b5cf6e332255698fc1b79a3bf8b9
minjeongss <dotoriido@gmail.com>
2025-01-21 00:40:39 +0900

    feat: Modal에 useOnClickOutside 적용

--- packages/components/Modal/Modal.tsx
+++ packages/components/Modal/Modal.tsx
@@ -8,15 +8,19 @@ import {
   ModalWrapper,
 } from "./Modal.styles";
 import { ModalProps } from "./Modal.d";
+import { useRef } from "react";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const Modal = ({ children, isOpen, onToggle }: ModalProps) => {
   const element = usePortal("modal");
+  const ref = useRef(null);
+  useOnClickOutside(ref, onToggle);
 
   if (!element) return null;
 
   return createPortal(
     isOpen ? (
-      <ModalWrapper>
+      <ModalWrapper ref={ref}>
         <ModalHeader>
           <CloseButton onClick={onToggle}>X</CloseButton>
         </ModalHeader>
