f67d16c436af08d66d666f49fd02b33dc7996ca2
minjeongss <dotoriido@gmail.com>
2025-02-26 17:40:38 +0900

    docs: Modal, Skeleton JSDoc 설명 추가

--- packages/components/Modal/Modal.tsx
+++ packages/components/Modal/Modal.tsx
@@ -11,6 +11,16 @@ import { useRef } from "react";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
 import useFadeOutMotion from "../../hooks/useFadeOutMotion";
 
+/**
+ * @description
+ * Portal을 활용한 Modal
+ *
+ * @param isOpen Modal의 열림/닫힘 여부
+ * @param onClose Modal을 닫는 함수
+ * @param header Modal의 상단 부분이며, string 또는 ReactNode 사용 가능
+ * @param body Modal의 내용 부분이며, string 또는 ReactNode 사용 가능
+ * @param position Modal이 나타나는 위치
+ */
 const Modal = ({
   isOpen,
   onClose,
--- packages/components/Skeleton/Skeleton.tsx
+++ packages/components/Skeleton/Skeleton.tsx
@@ -1,6 +1,13 @@
 import { SkeletonProps } from "./Skeleton.d";
 import { SkeletonWrapper, Shimmer } from "./Skeleton.styles";
 
+/**
+ * @description
+ * Skeleton UI
+ *
+ * @param width Skeleton의 너비
+ * @param height Skeleton의 높이
+ */
 const Skeleton = ({ width = 60, height = 60 }: SkeletonProps) => {
   return (
     <SkeletonWrapper width={width} height={height}>
